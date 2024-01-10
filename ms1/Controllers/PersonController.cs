f0c1fba51853028565f9a2522dc715174ddf1f86
favioOV <156195983+favioOV@users.noreply.github.com>
2024-01-10 09:36:41 -0300

    commit inicial

---
+++ ms1/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ms1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Person> items = new();
+
+            items.Add(new Person() { Name = "Pepe" });
+            items.Add(new Person() { Name = "Pipo" });
+
+            return Ok(items);
+        }
+    }
+
+    
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+        public string Address { get; set; }
+    }
+
+     
+}
---
+++ ms1/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ms1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var items = new List<string> { "Item1", "Item2", "Item3" };
+            return Ok(items);
+        }
+    }
+}
